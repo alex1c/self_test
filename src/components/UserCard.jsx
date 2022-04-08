6ae980356b60cd8c5ca3b5f45b3d20cf8e9bd44e
alex1c <alex1c.spb@gmail.com>
2022-04-08 18:45:27 +0300

    master

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,51 +2,44 @@ import React from "react";
 import { useState, useEffect } from "react";
 //import { useParams } from "@react/router"
 import ListGroup from "react-bootstrap/ListGroup";
-import { Link, useParams } from "react-router-dom";
-//let params = useParams();
-{
-  /* <div>UserCard {params.userId}</div> */
-}
+import { useParams } from "react-router-dom";
 
 const UserCard = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
-  ////let params = useParams();
   const params = useParams();
 
   // Примечание: пустой массив зависимостей [] означает, что
   // этот useEffect будет запущен один раз
   // аналогично componentDidMount()
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`)
+    //// fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`)
+    //захардкодил одного юзера для теста
+    fetch(`https://jsonplaceholder.typicode.com/users/5`)
       .then((res) => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
-          //setItems(result);
-          //setItems(Object.entries(result));
+
           const resultArray = Object.entries(result);
 
-          // resultArray.map((curit) => console.log(curit))
-          //const newArr = resultArray.slice()
           resultArray.map((resultArrayItem) => {
             if (typeof resultArrayItem[1] === "object") {
               resultArrayItem[1] = Object.entries(resultArrayItem[1]);
               resultArrayItem[1].map((temp) => {
                 if (typeof temp[1] === "object") {
                   temp[1] = Object.entries(temp[1]);
+                } else {
                 }
               });
+            } else {
             }
-            console.log(resultArrayItem);
           });
 
           setItems(resultArray);
-          //console.log(items)
-
-          //// setItems(result.map((el) => ({ ...el, myPole: "" })));
+          console.log("items ", items);
         },
         // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
         // чтобы не перехватывать исключения из ошибок в самих компонентах.
@@ -57,47 +50,47 @@ const UserCard = () => {
       );
   }, []);
 
-  console.log(items);
+  //console.log(items);
+  console.log("основной стейт с данными для вывода ", items);
 
-  let outputArray = []
-
-  
-   items.map((currentItems) => {
-   // console.log("1-", currentItems[1]);
-   outputArray.push(currentItems[0], currentItems[1])
-    if (Array.isArray(currentItems[1])) {
-     // console.log("2-", currentItems[1][1]);
-     outputArray.push(currentItems[1][0], currentItems[1][1])
-      currentItems[1].map((currentItems2) => {
-      //  console.log("3-", currentItems2[1]);
-
-        if (Array.isArray(currentItems2[1])) {
-          currentItems2[1].map((currentItems3) => {
-          //  console.log("4-", currentItems3[1]);
-          });
-        }
-      });
+  function getString(Myitem) {
+    try {
+      if (!Array.isArray(Myitem[1])) {
+        return Myitem[0] + "-эта строка рендерится--" + Myitem[1];
+      } else if (Array.isArray(Myitem[1])) {
+        Myitem[1].map((item2) => {
+          if (!Array.isArray(item2[0])) {
+            console.log(
+              "здесь данные в консоль выводятся, но не рендерятся ",
+              item2[0] + "--" + item2[1]
+            );
+            return item2[0] + "--" + item2[1];
+          } else return "also_hello";
+        });
+      } else {
+        return "hello";
+      }
+    } catch (error) {
+      console.log(error);
     }
-  }); 
+  }
 
   if (error) {
     return <div>Ошибка: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Загрузка...</div>;
   } else {
-    return <>
-    
-    {outputArray.map((rrr) => {
-
-<div>{rrr[1]  }</div>
-    })}
-    
-   
-  
-  
-    
-    
-    </>;
+    return (
+      <>
+        <ListGroup>
+          {items.map((item) => (
+            <ListGroup.Item key={item} id={item.id}>
+              {getString(item)}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </>
+    );
   }
 };
 
